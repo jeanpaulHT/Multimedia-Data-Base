33390147fe2a4148418e2a371ae60249bb57e396
davento <daniela.vento@utec.edu.pe>
2021-08-06 13:33:34 -0500

    fixed port

--- backend.py
+++ backend.py
@@ -41,5 +41,6 @@ def upload_image():
 
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+if __name__ == '__main__':
+    app.secret_key = ".."
+    app.run(port=8080, threaded=True, host=('127.0.0.1'))
